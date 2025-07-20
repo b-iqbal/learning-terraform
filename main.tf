3f2f13301844b3d9c65f9c4587145f7e80adae9c
Bilal <11413759+b-iqbal@users.noreply.github.com>
2025-07-20 22:32:11 +0000

    defined security group modules

--- main.tf
+++ main.tf
@@ -22,11 +22,26 @@ resource "aws_instance" "blog" {
   ami           = data.aws_ami.app_ami.id
   instance_type = var.instance_type
 
-  vpc_security_group_ids = [aws_security_group.blog_sg.id]
+  vpc_security_group_ids = [module.blog-new-security-group.security_group_id]
 
   tags = {
     Name = "Hi, There!"
   }
+}
+
+module "blog-new-security-group" {
+  source  = "terraform-aws-modules/security-group/aws"
+  version = "5.3.0"
+  name    = "blog_new_sg"
+
+  vpc_id = data.aws_vpc.default.id
+
+  ingress_rules = ["http-80-tcp", "https-443-tcp"]
+  ingress_cidr_blocks = ["0.0.0.0/0"]
+
+  # Allow all outbound traffic
+  egress_rules = ["all-all"]
+  egress_cidr_blocks = ["0.0.0.0/0"]
 }
 
 resource "aws_security_group" "blog_sg" {
