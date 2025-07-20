1c113e896d3cbffb8153a1c56767fd91ba83d96a
Bilal <11413759+b-iqbal@users.noreply.github.com>
2025-07-20 22:59:41 +0000

    issue with missing public subnet

--- main.tf
+++ main.tf
@@ -22,7 +22,7 @@ module "blog_vpc" {
   cidr = "10.0.0.0/16"
 
   azs             = ["eu-west-1", "eu-west-2", "eu-west-3"]
-  private_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
+  public_subnets = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
 
   tags = {
     Terraform = "true"
